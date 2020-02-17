96e8f51ee06a3f0ffbeefe10809707d288a71dad
kjuchniewicz <kjuchniewicz@zarna.pl>
2020-02-17 13:40:46 +0100

    try dash

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
+from dash import Dash
+import dash_core_components as dcc
+import dash_html_components as html
 from flask import Flask
 
-app = Flask(__name__)
+server = Flask(__name__)
+app_dash = Dash(__name__, server=server, url_base_pathname='/dashboard/')
+app_dash.layout = html.Div(
+    children=[
+        html.H1(children='Dashboard')
+    ]
+)
 
 
-@app.route('/')
-def hello_world():
+@server.route('/')
+def index():
     return 'Hello Vuko'
 
 
 if __name__ == '__main__':
-    app.run()
+    server.run()
